cae005e32657bfb7bb71ba60c782b395734ff32b
mustard221 <C24432034@mytudublin.ie>
2025-04-08 17:15:41 +0100

    rotation_fixed

--- melt-down!/Scenes/cone.gd
+++ melt-down!/Scenes/cone.gd
@@ -1,25 +1,22 @@
 extends Sprite2D
-# Tracking initial rotation and updating for every rotation
-var last_rotation := 0.0  
+
+var target_rotation = rotation  # initial target rotation
+var speed = 100
 
 func _process(delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
 	var Hdistance = mouse_pos.x - global_position.x
 	var Vdistance = mouse_pos.y - global_position.y
-
-	var target_rotation = rotation
-#rotating cone 
+	
 	if Hdistance > 45:
-		target_rotation = deg_to_rad(90)
+		target_rotation = deg_to_rad(90)  
 	elif Hdistance < -45:
-		target_rotation = deg_to_rad(-90)
-	elif Vdistance > 0:
-		target_rotation = deg_to_rad(180)
+		target_rotation = deg_to_rad(-90)  
 	elif Vdistance < 0:
-		target_rotation = deg_to_rad(0)
+		target_rotation = deg_to_rad(0) 
+	elif Vdistance > 10:
+		target_rotation = deg_to_rad(180) 
+	
 
-
-	if target_rotation != last_rotation:
-		last_rotation = target_rotation #Adds on previous rotation amount to new amount and repeats whenever rotated
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "rotation", target_rotation, 0.5).set_ease(Tween.EASE_IN)
+	var tween = get_tree().create_tween() #smoother rotating using lerp to calculate smallest angle
+	tween.tween_property(self, "rotation", lerp_angle(rotation, target_rotation, delta * speed), 0.25).set_trans(Tween.TRANS_LINEAR)
