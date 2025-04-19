4df79094c86cbbf3ce328f976b8707571b536935
mustard221 <C24432034@mytudublin.ie>
2025-04-19 14:25:32 +0100

    cooldown_shooting

--- melt-down!/Scripts/shoot.gd
+++ melt-down!/Scripts/shoot.gd
@@ -3,12 +3,23 @@ extends Marker2D
 @export var iceCream: PackedScene
 var cooldown := 0.0
 var delay := 0.05#seconds between shots
+var active = true
+var time = Vector2.ZERO
+
 
 func _physics_process(delta: float) -> void:
+	time = 0
 	cooldown -= delta
-	if Input.is_action_pressed("fire") and cooldown <= 0.0:
+	if Input.is_action_pressed("fire") and cooldown <= 0.0 and time < 0.9:
 		var shoot = iceCream.instantiate()
 		shoot.global_position = global_position
 		get_tree().current_scene.add_child(shoot)
 		print("spawns at: ", shoot.global_position)
 		cooldown = delay  # reset cooldown
+	else:
+		iceCream.queue_free()
+		_on_timer_timeout()
+
+func _on_timer_timeout() -> void:
+	time = 1.0
+	pass # Replace with function body.
